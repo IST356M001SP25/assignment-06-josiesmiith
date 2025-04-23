f9962d3d3e677dae72a1a4918183a8efa5cc0693
Josie Smith <jsmit194@syr.edu>
2025-04-22 22:49:45 -0400

    Part 1 complete

--- code/apicalls.py
+++ code/apicalls.py
@@ -1,19 +1,39 @@
 import requests
 
 # Put your CENT Ischool IoT Portal API KEY here.
-APIKEY = "APIKEYHERE"
+APIKEY = "bbb449446a3ebecd565bcd69"
 
 def get_google_place_details(google_place_id: str) -> dict:
-    pass # Implement this function
+    header = { 'X-API-KEY': APIKEY }
+    params = { 'place_id': google_place_id }
+    url = "https://cent.ischool-iot.net/api/google/places/details"
+    response = requests.get(url, headers=header, params=params)
+    response.raise_for_status()
+    return response.json()
     
 def get_azure_sentiment(text: str) -> dict:
-    pass # Implement this function
+    header = { 'X-API-KEY': APIKEY }
+    data = { "text" : text }
+    url = "https://cent.ischool-iot.net/api/azure/sentiment"
+    response = requests.post(url, headers=header, data=data)
+    response.raise_for_status()
+    return response.json()
 
 def get_azure_key_phrase_extraction(text: str) -> dict:
-    pass # Implement this function
+    header = {'X-API-KEY': APIKEY }
+    data = { "text": text }
+    url = "https://cent.ischool-iot.net/api/azure/keyphrasextraction"
+    response = requests.post(url, headers=header, data=data)
+    response.raise_for_status()
+    return response.json()
 
 def get_azure_named_entity_recognition(text: str) -> dict:
-    pass # Implement this function
+    header = { 'X-API-KEY': APIKEY }
+    data = { "text": text }
+    url = "https://cent.ischool-iot.net/api/azure/entityrecognition"
+    response = requests.post(url, headers=header, data=data)
+    response.raise_for_status()
+    return response.json()
 
 
 def geocode(place:str) -> dict:
@@ -26,7 +46,7 @@ def geocode(place:str) -> dict:
     url = "https://cent.ischool-iot.net/api/google/geocode"
     response = requests.get(url, headers=header, params=params)
     response.raise_for_status()
-    return response.json()  # Return the JSON response as a dictionary
+    return response.json() 
 
 
 def get_weather(lat: float, lon: float) -> dict:
@@ -39,4 +59,4 @@ def get_weather(lat: float, lon: float) -> dict:
     url = "https://cent.ischool-iot.net/api/weather/current"
     response = requests.get(url, headers=header, params=params)
     response.raise_for_status()
-    return response.json()  # Return the JSON response as a dictionary
+    return response.json() 
